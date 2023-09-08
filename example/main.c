ef414d0d0b14902e855673eb001a472743e2fded
Klavishnik <shtanov.klavishnik@yandex.ru>
2023-09-09 01:14:26 +0300

    add example

---
+++ example/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h> 
+#include <stdbool.h> 
+
+int isNull(int num) {
+    if (num == 0)
+        return 1;
+    return 0;
+}
+
+bool isNegative(int* num) {
+    if (*num < 0) {
+        *num *= -1;
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    int input, zero = 0, output = 0;
+    bool negative = false;
+
+    printf("Enter a number: ");
+    scanf("%d", &input);
+   
+    zero = isNull(input);
+    negative = isNegative(&input);
+
+    int mult = 1;
+    while (input > 0) {
+        if (input % 10 == 0 ) {
+            zero++;
+        }
+        else {
+            output += input % 10 * mult;
+            mult *= 10; 
+        }
+
+        input = input / 10;
+    }
+    if (negative == true)
+        output += -1; 
+
+    printf("Zero= %i\n", zero);
+    printf("Output dd= %i\n", output);
+    return 0;
+}
